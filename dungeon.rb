afbb3633d8fd7931bf2d7d52e915f6980a52363f
Edward Geist <sovietologist@gmail.com>
2016-02-24 09:46:17 -0800

    Add FILE= to #{opt_share}/dindx'

--- dungeon.rb
+++ dungeon.rb
@@ -10,7 +10,7 @@ class Dungeon < Formula
     chdir "src" do
       # look for game files where homebrew installed them, not pwd
       inreplace "game.f" do |s|
-        s.gsub! "FILE='dindx',STATUS='OLD',", "#{opt_share}/dindx',"
+        s.gsub! "FILE='dindx',STATUS='OLD',", "FILE=#{opt_share}/dindx',"
         s.gsub! "1	FORM='FORMATTED',ACCESS='SEQUENTIAL',ERR=1900)", "1	STATUS='OLD',FORM='FORMATTED',
 	2	ACCESS='SEQUENTIAL',ERR=1900)"
         s.gsub! "FILE='dtext',STATUS='OLD',", "FILE='#{opt_share}/dtext',"
